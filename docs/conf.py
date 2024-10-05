93b7df33448de8bc901bc22224d8578b4e4b922d
COBRAPROsimulator <159205140+COBRAPROsimulator@users.noreply.github.com>
2024-10-05 15:54:03 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,9 @@ release = 'v1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 source_suffix = '.rst'
